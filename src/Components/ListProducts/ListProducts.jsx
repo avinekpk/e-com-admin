c433302aaa4da2314df5511b91b31e78d53e81c4
avinek <avinek.p.kavil@gmail.com>
2024-04-18 05:15:57 +0530

    Add product done.

---
+++ src/Components/ListProducts/ListProducts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ListProducts.css";
+
+const ListProducts = () => {
+  return (
+    <div className="list-product">
+      <p></p>
+    </div>
+  );
+};
+
+export default ListProducts;
